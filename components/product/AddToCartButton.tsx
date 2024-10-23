b0adc8ca5df7d38a656afede49b939b959ea0042
Suporte Wing <suporte@wingcorp.com.br>
2024-10-23 09:21:43 -0300

    Publish 1 from lucascosta2 into origin/main

--- components/product/AddToCartButton.tsx
+++ components/product/AddToCartButton.tsx
@@ -18,14 +18,19 @@ const onClick = (inputId: string) => {
   // const container = button!.closest<HTMLDivElement>("div[data-cart-item]")!;
 
   const input = document.getElementById(inputId);
-  const inputValue = input!.querySelector<HTMLInputElement>("input[type=number]");
+  const inputValue =
+    input!.querySelector<HTMLInputElement>("input[type=number]");
   const container = input!.closest<HTMLDivElement>("div[data-cart-item]")!;
 
-  const { item, platformProps } = JSON.parse(decodeURIComponent(container.getAttribute("data-cart-item")!));
+  const { item, platformProps } = JSON.parse(
+    decodeURIComponent(container.getAttribute("data-cart-item")!)
+  );
   if (!inputValue) return;
   item.quantity = Number(inputValue.value);
   console.log("Add to cart", item, item.quantity);
-  const productId = inputValue!.closest("div[data-cart-item]")!.getAttribute("data-item-id")!;
+  const productId = inputValue!
+    .closest("div[data-cart-item]")!
+    .getAttribute("data-item-id")!;
   window.STOREFRONT.CART.addToCart(item, platformProps);
   window.STOREFRONT.CART.setQuantity(item.item_id, item.quantity);
 };
@@ -44,8 +49,12 @@ const onLoad = (id: string) => {
     // }
     // checkbox.checked = quantity > 0;
     // enable interactivity
-    container?.querySelectorAll<HTMLButtonElement>("button").forEach((node) => (node.disabled = false));
-    container?.querySelectorAll<HTMLButtonElement>("input").forEach((node) => (node.disabled = false));
+    container
+      ?.querySelectorAll<HTMLButtonElement>("button")
+      .forEach((node) => (node.disabled = false));
+    container
+      ?.querySelectorAll<HTMLButtonElement>("input")
+      .forEach((node) => (node.disabled = false));
   });
 };
 const useAddToCart = ({ product, seller }: Props) => {
@@ -65,7 +74,9 @@ const useAddToCart = ({ product, seller }: Props) => {
     return {
       quantity: 1,
       itemId: productID,
-      attributes: Object.fromEntries(additionalProperty.map(({ name, value }) => [name, value])),
+      attributes: Object.fromEntries(
+        additionalProperty.map(({ name, value }) => [name, value])
+      ),
     };
   }
   if (platform === "wake") {
@@ -79,7 +90,9 @@ const useAddToCart = ({ product, seller }: Props) => {
       quantity: 1,
       itemId: Number(productGroupID),
       add_to_cart_enhanced: "1",
-      attributes: Object.fromEntries(additionalProperty.map(({ name, value }) => [name, value])),
+      attributes: Object.fromEntries(
+        additionalProperty.map(({ name, value }) => [name, value])
+      ),
     };
   }
   if (platform === "linx") {
@@ -101,7 +114,9 @@ function AddToCartButton(props: Props) {
       id={id}
       class="flex"
       data-item-id={product.productID}
-      data-cart-item={encodeURIComponent(JSON.stringify({ item, platformProps }))}
+      data-cart-item={encodeURIComponent(
+        JSON.stringify({ item, platformProps })
+      )}
     >
       {/* <input type="checkbox" class="hidden peer" /> */}
 
@@ -113,13 +128,16 @@ function AddToCartButton(props: Props) {
         <Image
           src="https://deco-sites-assets.s3.sa-east-1.amazonaws.com/festval/d4f85472-4e59-4bc7-a533-792824538320/Repeticao-de-grade-2.svg"
           alt="carrinho"
-          width={device !== "mobile" ? 35 : 16}
-          height={device !== "mobile" ? 35 : 16}
+          width={16}
+          height={16}
           //class="mt-[-3px]"
         />
         Adicionar
       </button>
-      <script type="module" dangerouslySetInnerHTML={{ __html: useScript(onLoad, id) }} />
+      <script
+        type="module"
+        dangerouslySetInnerHTML={{ __html: useScript(onLoad, id) }}
+      />
     </div>
   );
 }
