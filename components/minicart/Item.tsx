f92037f05f56cb938068d4093ef3da9db24094fb
Suporte Wing <suporte@wingcorp.com.br>
2025-01-13 13:16:07 -0300

    Correção pesagem carrinho e pdp

--- components/minicart/Item.tsx
+++ components/minicart/Item.tsx
@@ -8,210 +8,214 @@ import { useScript } from "@deco/deco/hooks";
 import { useId } from "../../sdk/useId.ts";
 import QuantitySelectorKgCart from "../ui/QuantitySelectorKgCart.tsx";
 export type Item = AnalyticsItem & {
-  listPrice: number;
-  image: string;
-  unitMultiplier: number;
-  measurementUnit: string;
+	listPrice: number;
+	image: string;
+	unitMultiplier: number;
+	measurementUnit: string;
 };
 export interface Props {
-  item: Item;
-  index: number;
-  locale: string;
-  currency: string;
+	item: Item;
+	index: number;
+	locale: string;
+	currency: string;
 }
 
 interface ProductData {
-  ActivateIfPossible: boolean;
-  CommercialConditionId: number;
-  CreationDate: string;
-  CubicWeight: number;
-  EstimatedDateArrival: string | null;
-  Height: number | null;
-  Id: number;
-  IsActive: boolean;
-  IsKit: boolean;
-  KitItensSellApart: boolean;
-  Length: number | null;
-  ManufacturerCode: string;
-  MeasurementUnit: string;
-  ModalType: string | null;
-  Name: string;
-  PackagedHeight: number;
-  PackagedLength: number;
-  PackagedWeightKg: number;
-  PackagedWidth: number;
-  ProductId: number;
-  RefId: string;
-  RewardValue: number | null;
-  UnitMultiplier: number;
-  Videos: Array<unknown>;
-  WeightKg: number | null;
-  Width: number | null;
+	ActivateIfPossible: boolean;
+	CommercialConditionId: number;
+	CreationDate: string;
+	CubicWeight: number;
+	EstimatedDateArrival: string | null;
+	Height: number | null;
+	Id: number;
+	IsActive: boolean;
+	IsKit: boolean;
+	KitItensSellApart: boolean;
+	Length: number | null;
+	ManufacturerCode: string;
+	MeasurementUnit: string;
+	ModalType: string | null;
+	Name: string;
+	PackagedHeight: number;
+	PackagedLength: number;
+	PackagedWeightKg: number;
+	PackagedWidth: number;
+	ProductId: number;
+	RefId: string;
+	RewardValue: number | null;
+	UnitMultiplier: number;
+	Videos: Array<unknown>;
+	WeightKg: number | null;
+	Width: number | null;
 }
 
 const onLoad = async (id: string, item: Item, quantity: number) => {
-  async function getProductData(itemId: string): Promise<ProductData | null> {
-    const url = `https://www.integracaoiota.com.br/festval-deco-helpers/index.php?skuId=${itemId}`;
+	async function getProductData(itemId: string): Promise<ProductData | null> {
+		const url = `https://www.integracaoiota.com.br/festval-deco-helpers/index.php?skuId=${itemId}`;
 
-    try {
-      const response = await fetch(url);
+		try {
+			const response = await fetch(url);
 
-      if (!response.ok) {
-        throw new Error(`Erro ao buscar o SKU: ${response.statusText}`);
-      }
+			if (!response.ok) {
+				throw new Error(`Erro ao buscar o SKU: ${response.statusText}`);
+			}
 
-      const data = await response.json();
-      return data;
-    } catch (error) {
-      console.error("Erro ao buscar os dados:", error);
-      return null;
-    }
-  }
+			const data = await response.json();
+			return data;
+		} catch (error) {
+			console.error("Erro ao buscar os dados:", error);
+			return null;
+		}
+	}
 
-  const itemWithId = item as Item & { item_id: string };
-  const productData = await getProductData(itemWithId.item_id);
+	const itemWithId = item as Item & { item_id: string };
+	const productData = await getProductData(itemWithId.item_id);
 
-  const container = document.getElementById(id);
+	const container = document.getElementById(id);
 
-  if (productData && productData.MeasurementUnit === "kg") {
-    const divInput = document.getElementById(`input-${id}`);
-    divInput?.setAttribute("data-product-data", JSON.stringify(productData));
-    const input =
-      divInput?.querySelector<HTMLInputElement>('input[type="text"]');
-    divInput?.setAttribute("data-quantity-number", quantity.toString());
-    input?.setAttribute("data-product-data", JSON.stringify(productData));
-    input?.setAttribute("data-quantity-number", quantity.toString());
+	if (productData && productData.MeasurementUnit === "kg") {
+		const divInput = document.getElementById(`input-${id}`);
+		divInput?.setAttribute("data-product-data", JSON.stringify(productData));
+		const input =
+			divInput?.querySelector<HTMLInputElement>('input[type="text"]');
+		divInput?.setAttribute("data-quantity-number", quantity.toString());
+		input?.setAttribute("data-product-data", JSON.stringify(productData));
+		input?.setAttribute("data-quantity-number", quantity.toString());
 
-    if (productData && productData.UnitMultiplier) {
-      const quantityForInput = (productData.UnitMultiplier * quantity).toFixed(
-        3
-      );
-      if (input) {
-        input.value = `${quantityForInput} kg`;
-      }
-    }
-  }
+		if (productData && productData.UnitMultiplier) {
+			const quantityForInput = (productData.UnitMultiplier * quantity).toFixed(
+				3
+			);
+			if (input) {
+				input.value = `${quantityForInput} kg`;
+			}
+		}
+	}
 };
 
 const QUANTITY_MAX_VALUE = 100;
 const removeItemHandler = (measurementUnit: string) => {
-  const itemID = (event?.currentTarget as HTMLButtonElement | null)
-    ?.closest("fieldset")
-    ?.getAttribute("data-item-id");
+	const itemID = (event?.currentTarget as HTMLButtonElement | null)
+		?.closest("fieldset")
+		?.getAttribute("data-item-id");
 
-  if (typeof itemID === "string") {
-    if (measurementUnit === "kg") {
-      window.STOREFRONT.CART.setQuantity(itemID, 0, true);
-    } else {
-      window.STOREFRONT.CART.setQuantity(itemID, 0, false);
-    }
+	if (typeof itemID === "string") {
+		if (measurementUnit === "kg") {
+			window.STOREFRONT.CART.setQuantity(itemID, 0, true);
+		} else {
+			window.STOREFRONT.CART.setQuantity(itemID, 0, false);
+		}
 
-    const buttonsAddToCart = document.querySelectorAll<HTMLButtonElement>(
-      'button[data-attribute="add-to-cart"]'
-    );
+		const buttonsAddToCart = document.querySelectorAll<HTMLButtonElement>(
+			'button[data-attribute="add-to-cart"]'
+		);
 
-    const matchingButtons = Array.from(buttonsAddToCart).filter(
-      (button) => button.getAttribute("data-item-id") === itemID
-    );
+		const matchingButtons = Array.from(buttonsAddToCart).filter(
+			(button) => button.getAttribute("data-item-id") === itemID
+		);
 
-    matchingButtons.forEach((button) => {
-      button.style.backgroundColor = "#5D7F3A";
-      button.style.color = "#fff";
-      button.style.border = "1px solid  #5D7F3A";
-      button.innerText = "Adicionar";
+		matchingButtons.forEach((button) => {
+			button.style.backgroundColor = "#5D7F3A";
+			button.style.color = "#fff";
+			button.style.border = "1px solid  #5D7F3A";
+			button.innerText = "Adicionar";
 
-      button.disabled = true;
-    });
-  }
+			button.disabled = true;
+		});
+	}
 };
 
 function CartItem({ item, index, locale, currency }: Props) {
-  const {
-    image,
-    listPrice,
-    price = Infinity,
-    quantity,
-    measurementUnit,
-  } = item;
-  const isGift = price < 0.01;
-  // deno-lint-ignore no-explicit-any
-  const name = (item as any).item_name;
-  const id = useId();
+	const {
+		image,
+		listPrice,
+		price = Infinity,
+		quantity,
+		measurementUnit,
+	} = item;
+	const isGift = price < 0.01;
+	// deno-lint-ignore no-explicit-any
+	const name = (item as any).item_name;
+	const id = useId();
 
-  return (
-    <fieldset
-      // deno-lint-ignore no-explicit-any
-      id={id}
-      data-item-id={(item as any).item_id}
-      class="grid grid-rows-1 gap-2"
-      style={{ gridTemplateColumns: "auto 1fr" }}
-    >
-      <Image
-        alt={name}
-        src={image.replace("55-55", "100-100")}
-        width={100}
-        height={100}
-        class="object-center object-contain lg:w-[107px] lg:h-[107px] mix-blend-multiply"
-      />
-      {/* Info */}
-      <div class="flex flex-col gap-2">
-        {/* Name and Remove button */}
-        <div class="flex justify-between items-center">
-          <legend class="font-semibold">{name}</legend>
-          <button
-            class={clx(
-              isGift && "hidden",
-              "btn btn-ghost hover:bg-transparent hover:opacity-80 btn-square no-animation"
-            )}
-            hx-on:click={useScript(removeItemHandler, measurementUnit)}
-          >
-            <Icon id="trash" size={24} />
-          </button>
-        </div>
+	return (
+		<fieldset
+			// deno-lint-ignore no-explicit-any
+			id={id}
+			data-item-id={(item as any).item_id}
+			class="grid grid-rows-1 gap-2"
+			style={{ gridTemplateColumns: "auto 1fr" }}
+		>
+			<Image
+				alt={name}
+				src={image.replace("55-55", "100-100")}
+				width={100}
+				height={100}
+				class="object-center object-contain lg:w-[107px] lg:h-[107px] mix-blend-multiply"
+			/>
+			{/* Info */}
+			<div class="flex flex-col gap-2">
+				{/* Name and Remove button */}
+				<div class="flex justify-between items-center">
+					<legend class="font-semibold">{name}</legend>
+					<button
+						class={clx(
+							isGift && "hidden",
+							"btn btn-ghost hover:bg-transparent hover:opacity-80 btn-square no-animation"
+						)}
+						hx-on:click={useScript(removeItemHandler, measurementUnit)}
+					>
+						<Icon id="trash" size={24} />
+					</button>
+				</div>
 
-        {/* Price Block */}
-        <div class="flex items-start gap-1 flex-col">
-          {listPrice && price && listPrice > price && (
-            <span class="line-through text-sm list-price">
-              {formatPrice(listPrice, currency, locale)}
-            </span>
-          )}
-          <span class="text-lg font-medium text-[#282828] price">
-            {isGift ? "Grátis" : formatPrice(price, currency, locale)}
-          </span>
-        </div>
+				{/* Price Block */}
+				<div class="flex items-start gap-1 flex-col">
+					{listPrice && price && listPrice > price && (
+						<span class="line-through text-sm list-price">
+							{formatPrice(listPrice, currency, locale)}
+						</span>
+					)}
+					<span class="text-lg font-medium text-[#282828] price">
+						{isGift
+							? "Grátis"
+							: measurementUnit == "kg"
+							? `${formatPrice(price, currency, locale)} Kg`
+							: formatPrice(price, currency, locale)}
+					</span>
+				</div>
 
-        {/* Quantity Selector */}
-        <div>
-          {measurementUnit === "kg" ? (
-            <div id={`input-${id}`} class={clx("quantity-cart-kg")}>
-              <QuantitySelectorKgCart
-                id={`input-${id}`}
-                min={0}
-                max={QUANTITY_MAX_VALUE}
-                value={quantity}
-                name={`item::${index}`}
-              />
-            </div>
-          ) : (
-            <div id={`input-${id}`} class={clx("quantity-cart-normal")}>
-              <QuantitySelector
-                min={0}
-                max={QUANTITY_MAX_VALUE}
-                value={quantity}
-                name={`item::${index}`}
-              />
-            </div>
-          )}
-        </div>
-      </div>
-      <script
-        type="module"
-        dangerouslySetInnerHTML={{
-          __html: useScript(onLoad, id, item, quantity),
-        }}
-      />
-    </fieldset>
-  );
+				{/* Quantity Selector */}
+				<div>
+					{measurementUnit === "kg" ? (
+						<div id={`input-${id}`} class={clx("quantity-cart-kg")}>
+							<QuantitySelectorKgCart
+								id={`input-${id}`}
+								min={0}
+								max={QUANTITY_MAX_VALUE}
+								value={quantity}
+								name={`item::${index}`}
+							/>
+						</div>
+					) : (
+						<div id={`input-${id}`} class={clx("quantity-cart-normal")}>
+							<QuantitySelector
+								min={0}
+								max={QUANTITY_MAX_VALUE}
+								value={quantity}
+								name={`item::${index}`}
+							/>
+						</div>
+					)}
+				</div>
+			</div>
+			<script
+				type="module"
+				dangerouslySetInnerHTML={{
+					__html: useScript(onLoad, id, item, quantity),
+				}}
+			/>
+		</fieldset>
+	);
 }
 export default CartItem;
--- components/ui/QuantitySelectorKg.tsx
+++ components/ui/QuantitySelectorKg.tsx
@@ -4,169 +4,170 @@ import { useScript } from "@deco/deco/hooks";
 import Image from "apps/website/components/Image.tsx";
 
 interface ProductData {
-  ActivateIfPossible: boolean;
-  CommercialConditionId: number;
-  CreationDate: string;
-  CubicWeight: number;
-  EstimatedDateArrival: string | null;
-  Height: number | null;
-  Id: number;
-  IsActive: boolean;
-  IsKit: boolean;
-  KitItensSellApart: boolean;
-  Length: number | null;
-  ManufacturerCode: string;
-  MeasurementUnit: string;
-  ModalType: string | null;
-  Name: string;
-  PackagedHeight: number;
-  PackagedLength: number;
-  PackagedWeightKg: number;
-  PackagedWidth: number;
-  ProductId: number;
-  RefId: string;
-  RewardValue: number | null;
-  UnitMultiplier: number;
-  Videos: Array<unknown>;
-  WeightKg: number | null;
-  Width: number | null;
+	ActivateIfPossible: boolean;
+	CommercialConditionId: number;
+	CreationDate: string;
+	CubicWeight: number;
+	EstimatedDateArrival: string | null;
+	Height: number | null;
+	Id: number;
+	IsActive: boolean;
+	IsKit: boolean;
+	KitItensSellApart: boolean;
+	Length: number | null;
+	ManufacturerCode: string;
+	MeasurementUnit: string;
+	ModalType: string | null;
+	Name: string;
+	PackagedHeight: number;
+	PackagedLength: number;
+	PackagedWeightKg: number;
+	PackagedWidth: number;
+	ProductId: number;
+	RefId: string;
+	RewardValue: number | null;
+	UnitMultiplier: number;
+	Videos: Array<unknown>;
+	WeightKg: number | null;
+	Width: number | null;
 }
 
 const onLoad = (id: string | null, maxAttempts = 5, delay = 1000) => {
-  let attempts = 0;
+	let attempts = 0;
 
-  function attemptLoad() {
-    if (!id) return;
+	function attemptLoad() {
+		if (!id) return;
 
-    const input = document.getElementById(id);
+		const input = document.getElementById(id);
 
-    const productData: ProductData = JSON.parse(
-      input?.getAttribute("data-product-data") ?? "{}"
-    );
+		const productData: ProductData = JSON.parse(
+			input?.getAttribute("data-product-data") ?? "{}"
+		);
 
-    if (Object.keys(productData).length === 0 && attempts < maxAttempts) {
-      attempts += 1;
-      setTimeout(attemptLoad, delay);
-    } else if (
-      Object.keys(productData).length > 0 &&
-      productData.MeasurementUnit === "kg"
-    ) {
-      if (input instanceof HTMLInputElement) {
-        const divInput = input!.closest(
-          ".input-div-container"
-        ) as HTMLDivElement;
-        const unitMultiplierMsg = divInput!.querySelector<HTMLSpanElement>(
-          "#unit-multiplier-msg"
-        );
-        const unitQuantityMsg =
-          divInput!.querySelector<HTMLSpanElement>("#unit-quantity-msg");
-        const quantity = input.getAttribute("data-quantity-number");
-        if (unitMultiplierMsg) {
-          unitMultiplierMsg.classList.remove("hidden");
-          unitQuantityMsg!.classList.remove("hidden");
-          unitMultiplierMsg.innerHTML = `Aprox. ${productData.UnitMultiplier.toFixed(
-            3
-          )} kg/unidade`;
-          unitQuantityMsg!.innerHTML = `Aprox. ${quantity} unidade(s) selecionada(s)`;
-        }
-      } else if (input instanceof HTMLDivElement) {
-        const inputElement = input!.querySelector<HTMLInputElement>(
-          'input[type="text"].input-kg'
-        );
-        // const divInput = input!.querySelector(
-        //   ".input-div-container"
-        // ) as HTMLDivElement;
-        const unitMultiplierMsg = inputElement!.closest(
-          "#unit-multiplier-msg"
-        ) as HTMLSpanElement;
-        const unitQuantityMsg = inputElement!.closest(
-          "#unit-quantity-msg"
-        ) as HTMLSpanElement;
-        const quantity = inputElement!.getAttribute("data-quantity-number");
-        if (unitMultiplierMsg) {
-          unitMultiplierMsg.classList.remove("hidden");
-          unitQuantityMsg!.classList.remove("hidden");
-          unitMultiplierMsg.innerHTML = `Aprox. ${productData.UnitMultiplier.toFixed(
-            3
-          )} kg/unidade`;
-          unitQuantityMsg!.innerHTML = `Aprox. ${quantity} unidade(s) selecionada(s)`;
-        }
-      }
-    } else if (
-      attempts == maxAttempts &&
-      productData.MeasurementUnit === "kg"
-    ) {
-      console.error(
-        "Erro: Limite máximo de tentativas atingido. Falha ao carregar productData."
-      );
-    }
-  }
+		if (Object.keys(productData).length === 0 && attempts < maxAttempts) {
+			attempts += 1;
+			setTimeout(attemptLoad, delay);
+		} else if (
+			Object.keys(productData).length > 0 &&
+			productData.MeasurementUnit === "kg"
+		) {
+			if (input instanceof HTMLInputElement) {
+				const divInput = input!.closest(
+					".input-div-container"
+				) as HTMLDivElement;
+				const unitMultiplierMsg = divInput!.querySelector<HTMLSpanElement>(
+					"#unit-multiplier-msg"
+				);
+				const unitQuantityMsg =
+					divInput!.querySelector<HTMLSpanElement>("#unit-quantity-msg");
+				const quantity = input.getAttribute("data-quantity-number");
+				if (unitMultiplierMsg) {
+					unitMultiplierMsg.classList.remove("hidden");
+					unitQuantityMsg!.classList.remove("hidden");
+					unitMultiplierMsg.innerHTML = `Aprox. ${productData.UnitMultiplier.toFixed(
+						3
+					)} kg/unidade`;
+					unitQuantityMsg!.innerHTML = `Aprox. ${quantity} unidade(s) selecionada(s)`;
+				}
+			} else if (input instanceof HTMLDivElement) {
+				const inputElement = input!.querySelector<HTMLInputElement>(
+					'input[type="text"].input-kg'
+				);
+				// const divInput = input!.querySelector(
+				//   ".input-div-container"
+				// ) as HTMLDivElement;
+				const unitMultiplierMsg = inputElement!.closest(
+					"#unit-multiplier-msg"
+				) as HTMLSpanElement;
+				const unitQuantityMsg = inputElement!.closest(
+					"#unit-quantity-msg"
+				) as HTMLSpanElement;
+				const quantity = inputElement!.getAttribute("data-quantity-number");
+				if (unitMultiplierMsg) {
+					unitMultiplierMsg.classList.remove("hidden");
+					unitQuantityMsg!.classList.remove("hidden");
+					unitMultiplierMsg.innerHTML = `Aprox. ${productData.UnitMultiplier.toFixed(
+						3
+					)} kg/unidade`;
 
-  attemptLoad();
+					unitQuantityMsg!.innerHTML = `Aprox. ${quantity} unidade(s) selecionada(s)`;
+				}
+			}
+		} else if (
+			attempts == maxAttempts &&
+			productData.MeasurementUnit === "kg"
+		) {
+			console.error(
+				"Erro: Limite máximo de tentativas atingido. Falha ao carregar productData."
+			);
+		}
+	}
+
+	attemptLoad();
 };
 
 const onClick = (delta: number) => {
-  event!.stopPropagation();
+	event!.stopPropagation();
 
-  const button = event!.currentTarget as HTMLButtonElement;
-  const input =
-    button.parentElement?.querySelector<HTMLInputElement>(
-      'input[type="text"]'
-    )!;
+	const button = event!.currentTarget as HTMLButtonElement;
+	const input =
+		button.parentElement?.querySelector<HTMLInputElement>(
+			'input[type="text"]'
+		)!;
 
-  const productData: ProductData = JSON.parse(
-    input.getAttribute("data-product-data") ?? "{}"
-  );
+	const productData: ProductData = JSON.parse(
+		input.getAttribute("data-product-data") ?? "{}"
+	);
 
-  const productQuantityHandler: number = parseInt(
-    input.getAttribute("data-quantity-number") ?? "1",
-    10
-  );
+	const productQuantityHandler: number = parseInt(
+		input.getAttribute("data-quantity-number") ?? "1",
+		10
+	);
 
-  const min = productData.UnitMultiplier * Number(input.min) || -Infinity;
-  const max = productData.UnitMultiplier * Number(input.max) || Infinity;
+	const min = productData.UnitMultiplier * Number(input.min) || -Infinity;
+	const max = productData.UnitMultiplier * Number(input.max) || Infinity;
 
-  const inputValueAsNumber = parseFloat(productData.UnitMultiplier.toFixed(3));
+	const inputValueAsNumber = parseFloat(productData.UnitMultiplier.toFixed(3));
 
-  input.value = `${Math.min(
-    Math.max(inputValueAsNumber * (productQuantityHandler + delta), min),
-    max
-  ).toFixed(3)} kg`;
+	input.value = `${Math.min(
+		Math.max(inputValueAsNumber * (productQuantityHandler + delta), min),
+		max
+	).toFixed(3)} kg`;
 
-  if (productQuantityHandler + delta <= 0) return;
+	if (productQuantityHandler + delta <= 0) return;
 
-  input.setAttribute(
-    "data-quantity-number",
-    (productQuantityHandler + delta).toString()
-  );
+	input.setAttribute(
+		"data-quantity-number",
+		(productQuantityHandler + delta).toString()
+	);
 
-  const unitQuantityMsg = input!
-    .closest(".input-div-container")
-    ?.querySelector<HTMLSpanElement>("#unit-quantity-msg");
-  unitQuantityMsg!.innerHTML = `Aprox. ${
-    productQuantityHandler + delta
-  } unidade(s) selecionada(s)`;
+	const unitQuantityMsg = input!
+		.closest(".input-div-container")
+		?.querySelector<HTMLSpanElement>("#unit-quantity-msg");
+	unitQuantityMsg!.innerHTML = `Aprox. ${
+		productQuantityHandler + delta
+	} unidade(s) selecionada(s)`;
 
-  const dataCartItem = input.closest("div[data-cart-item]");
-  const dataFieldset = input.closest("fieldset");
+	const dataCartItem = input.closest("div[data-cart-item]");
+	const dataFieldset = input.closest("fieldset");
 
-  let productId = "";
+	let productId = "";
 
-  if (dataCartItem?.getAttribute("data-item-id")) {
-    productId = dataCartItem.getAttribute("data-item-id")!;
-  } else if (dataFieldset?.getAttribute("data-item-id")) {
-    productId = dataFieldset.getAttribute("data-item-id")!;
-  }
+	if (dataCartItem?.getAttribute("data-item-id")) {
+		productId = dataCartItem.getAttribute("data-item-id")!;
+	} else if (dataFieldset?.getAttribute("data-item-id")) {
+		productId = dataFieldset.getAttribute("data-item-id")!;
+	}
 
-  if (productId) {
-    window.STOREFRONT.CART.setQuantity(
-      productId,
-      Number(input.getAttribute("data-quantity-number") || "1"),
-      true
-    );
-  }
+	if (productId) {
+		window.STOREFRONT.CART.setQuantity(
+			productId,
+			Number(input.getAttribute("data-quantity-number") || "1"),
+			true
+		);
+	}
 
-  input.dispatchEvent(new Event("change", { bubbles: true }));
+	input.dispatchEvent(new Event("change", { bubbles: true }));
 };
 
 // const onChange = () => {
@@ -185,80 +186,83 @@ const onClick = (delta: number) => {
 // };
 
 function QuantitySelectorKg({
-  id,
-  disabled,
-  ...props
+	id,
+	disabled,
+	...props
 }: JSX.IntrinsicElements["input"]) {
-  return (
-    <div class="flex flex-col items-start input-div-container">
-      <span
-        id="unit-multiplier-msg"
-        class="text-xs text-[#646072]  text-center "
-      >
-        Aprox. 800g/unidade
-      </span>
-      <span
-        id="unit-quantity-msg"
-        class="text-xs text-[#646072] text-center mb-1"
-      >
-        Aprox. 1 unidade(s) selecionada(s)
-      </span>
-      <div class="join w-full flex gap-[7px]">
-        <button
-          type="button"
-          class="w-[40px] h-[40px] bg-[#EBEAED] rounded-full	text-lg	flex justify-center items-center hover:opacity-80 ease-in-out duration-300"
-          hx-on:click={useScript(onClick, -1)}
-          disabled={disabled}
-        >
-          <Image
-            src="https://deco-sites-assets.s3.sa-east-1.amazonaws.com/festval/ebdd17e3-35f9-42a7-80ec-6489fae4aa14/menos.svg"
-            width={20}
-            height={20}
-          />
-        </button>
-        <div
-          //data-tip={`Quantity must be between ${props.min} and ${props.max}`}
-          class={clx(
-            "flex-grow join-item",
-            "flex flex-col justify-center items-center",
-            "has-[:invalid]:tooltip has-[:invalid]:tooltip-error has-[:invalid]:tooltip-open has-[:invalid]:tooltip-bottom"
-          )}
-        >
-          <input id="input-quantity-control" type="hidden" {...props} />
-          <input
-            id={id}
-            class={clx(
-              "input w-[100%] h-[40px] p-0 text-center flex-grow [appearance:textfield] border border-[#E3E3E3] rounded-[6px] text-[17px] font-bold",
-              "invalid:input-error",
-              "input-kg"
-            )}
-            disabled={disabled}
-            type="text"
-            readonly
-            {...props}
-          />
-        </div>
-        <button
-          type="button"
-          class="w-[40px] h-[40px] bg-[#EBEAED] rounded-full	text-lg flex justify-center items-center hover:opacity-80 ease-in-out duration-300"
-          hx-on:click={useScript(onClick, 1)}
-          disabled={disabled}
-        >
-          <Image
-            src="https://deco-sites-assets.s3.sa-east-1.amazonaws.com/festval/dbd42389-3ec3-4160-946e-2370117e9b95/mais.svg"
-            width={20}
-            height={20}
-            preload={true}
-          />
-        </button>
-      </div>
-      <script
-        type="module"
-        dangerouslySetInnerHTML={{
-          __html: useScript(onLoad, typeof id === "string" ? id : null),
-        }}
-      />
-    </div>
-  );
+	return (
+		<div
+			id="unit-multiplier-container"
+			class="flex flex-col items-start input-div-container"
+		>
+			<span
+				id="unit-multiplier-msg"
+				class="text-xs text-[#646072]  text-center "
+			>
+				Aprox. 800g/unidade
+			</span>
+			<span
+				id="unit-quantity-msg"
+				class="text-xs text-[#646072] text-center mb-1"
+			>
+				Aprox. 1 unidade(s) selecionada(s)
+			</span>
+			<div class="join w-full flex gap-[7px]">
+				<button
+					type="button"
+					class="w-[40px] h-[40px] bg-[#EBEAED] rounded-full	text-lg	flex justify-center items-center hover:opacity-80 ease-in-out duration-300"
+					hx-on:click={useScript(onClick, -1)}
+					disabled={disabled}
+				>
+					<Image
+						src="https://deco-sites-assets.s3.sa-east-1.amazonaws.com/festval/ebdd17e3-35f9-42a7-80ec-6489fae4aa14/menos.svg"
+						width={20}
+						height={20}
+					/>
+				</button>
+				<div
+					//data-tip={`Quantity must be between ${props.min} and ${props.max}`}
+					class={clx(
+						"flex-grow join-item",
+						"flex flex-col justify-center items-center",
+						"has-[:invalid]:tooltip has-[:invalid]:tooltip-error has-[:invalid]:tooltip-open has-[:invalid]:tooltip-bottom"
+					)}
+				>
+					<input id="input-quantity-control" type="hidden" {...props} />
+					<input
+						id={id}
+						class={clx(
+							"input w-[100%] h-[40px] p-0 text-center flex-grow [appearance:textfield] border border-[#E3E3E3] rounded-[6px] text-[17px] font-bold",
+							"invalid:input-error",
+							"input-kg"
+						)}
+						disabled={disabled}
+						type="text"
+						readonly
+						{...props}
+					/>
+				</div>
+				<button
+					type="button"
+					class="w-[40px] h-[40px] bg-[#EBEAED] rounded-full	text-lg flex justify-center items-center hover:opacity-80 ease-in-out duration-300"
+					hx-on:click={useScript(onClick, 1)}
+					disabled={disabled}
+				>
+					<Image
+						src="https://deco-sites-assets.s3.sa-east-1.amazonaws.com/festval/dbd42389-3ec3-4160-946e-2370117e9b95/mais.svg"
+						width={20}
+						height={20}
+						preload={true}
+					/>
+				</button>
+			</div>
+			<script
+				type="module"
+				dangerouslySetInnerHTML={{
+					__html: useScript(onLoad, typeof id === "string" ? id : null),
+				}}
+			/>
+		</div>
+	);
 }
 export default QuantitySelectorKg;
