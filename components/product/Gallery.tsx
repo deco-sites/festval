1eec8eb86bdd8b1d3852a820d6c41165bb5ac9e9
Suporte Wing <suporte@wingcorp.com.br>
2025-03-26 10:34:01 -0300

    Publish 1 from lucascosta2 into origin/main

--- components/product/Gallery.tsx
+++ components/product/Gallery.tsx
@@ -33,28 +33,37 @@ export default function GallerySlider(props: Props) {
 
   const {
     page: {
-      product: { name, isVariantOf, image: pImages, productID },
+      product: { name, isVariantOf, image: pImages },
     },
   } = props;
 
   const groupImages = isVariantOf?.image ?? pImages ?? [];
-  const filtered = groupImages.filter((img) => name?.includes(img.alternateName || ""));
-  const images = filtered.length > 0 ? filtered : groupImages;
+  // const filtered = groupImages.filter((img) =>
+  //   name?.includes(img.alternateName || "")
+  // );
+  // const images = filtered.length > 0 ? filtered : groupImages;
+  const images = groupImages;
 
   // Criar o item usando mapProductToAnalyticsItem
-  const item = mapProductToAnalyticsItem({
-    product: props.page.product,
-  });
+  // const item = mapProductToAnalyticsItem({
+  //   product: props.page.product,
+  // });
 
   return (
     <>
-      <div id={id} class="grid grid-flow-row sm:grid-flow-col grid-cols-1 sm:grid-cols-[min-content_1fr] ">
+      <div
+        id={id}
+        class="grid grid-flow-row sm:grid-flow-col grid-cols-1 sm:grid-cols-[min-content_1fr] "
+      >
         {/* Image Slider */}
         <div class="col-start-1 col-span-1 sm:col-start-2">
           <div class="relative  h-min flex-grow">
             <Slider class="carousel carousel-center gap-6 w-full">
               {images.map((img, index) => (
-                <Slider.Item index={index} class="carousel-item justify-center w-full">
+                <Slider.Item
+                  index={index}
+                  class="carousel-item justify-center w-full"
+                >
                   <Image
                     class="md:w-4/5 sm:w-full"
                     sizes="(max-width: 640px) 100vw, 100vw"
@@ -84,7 +93,12 @@ export default function GallerySlider(props: Props) {
               <Icon id="chevron-right" />
             </Slider.NextButton>
 
-            <ul class={clx("col-span-full absolute -bottom-2  inset-x-0 z-10", "carousel justify-center gap-3")}>
+            <ul
+              class={clx(
+                "col-span-full absolute -bottom-2  inset-x-0 z-10",
+                "carousel justify-center gap-3"
+              )}
+            >
               {images.map((_, index) => (
                 <li class="carousel-item">
                   <Slider.Dot
@@ -102,7 +116,12 @@ export default function GallerySlider(props: Props) {
 
         <Slider.JS rootId={id} />
       </div>
-      <ProductImageZoom id={zoomId} images={images} width={700} height={Math.trunc((700 * HEIGHT) / WIDTH)} />
+      <ProductImageZoom
+        id={zoomId}
+        images={images}
+        width={700}
+        height={Math.trunc((700 * HEIGHT) / WIDTH)}
+      />
     </>
   );
 }
